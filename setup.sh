acc76b1bc82761116e3e342847acc390ceeca1a7
paxton! <103937192+paxtonwarin@users.noreply.github.com>
2023-07-08 13:52:29 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 npm install
 rm -rf frontend
 rm -rf public
-git clone https://github.com/Abyss-Services/frontend -b losangeles1
+git clone https://github.com/Abyss-Services/frontend -b miami2
 cd frontend
 npm install
 npm run build
