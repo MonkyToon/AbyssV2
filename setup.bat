40fc4595120056295f3149d753bbb5bfc440978b
paxton! <103937192+paxtonwarin@users.noreply.github.com>
2023-07-08 13:47:58 -0400

    Delete setup.bat

--- setup.bat
+++
@@ -1,9 +0,0 @@
-CALL npm install
-CALL rmdir frontend /s /q 
-CALL rmdir public /s /q 
-CALL git clone https://github.com/Abyss-Services/frontend 
-CALL cd frontend 
-CALL npm install 
-CALL npm run build 
-CALL move ./public ../public 
-CALL cd .. 
